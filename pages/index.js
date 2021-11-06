74714f095553bb5429477636316bccf4fe9900d7
Nico Stranquist <nstranquist@gmail.com>
2021-11-05 23:46:52 -0500

    get user id and POST /users

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,37 @@
+import { useState } from 'react'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import logo from '../public/logo.svg'
 
 export default function Home() {
+  const [userId, setUserId] = useState("")
+  const [errors, setErrors] = useState(null)
+
+  const handleChange = e => {
+    setUserId(e.target.value)
+  }
+
+  const handleSubmit = e => {
+    e.preventDefault()
+    console.log(userId)
+
+    // fetch call to POST api at /api/users
+    // fetch('/api/users', {
+    //   method: 'POST',
+    //   headers: {
+    //     'Content-Type': 'application/json'
+    //   },
+    //   body: JSON.stringify({ userId })
+    // })
+    //   .then(res => res.json())
+    //   .then(data => console.log(data))
+    //   .catch(err => {
+    //     console.log(err)
+    //     setErrors(err)
+    //   })
+  }
+
   return (
     <div className={styles.container}>
       <Head>
@@ -12,21 +41,14 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        Hello World
+        <h1>Welcome to <Image src={logo} alt="flash pass logo" /> Flash Pass</h1>
+        <form onSubmit={handleSubmit}>
+          {errors && <p style={{color: 'red', opacity: 0.8}}>{errors.toString()}</p>}
+          <label htmlFor="user-id">User ID</label>
+          <input id="user-id" name="user-id" type="text" required value={userId} onChange={handleChange} />
+          <button type="submit">Submit</button>
+        </form>
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
     </div>
   )
 }
